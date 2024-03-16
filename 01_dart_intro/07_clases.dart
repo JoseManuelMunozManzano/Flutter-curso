edf6a5548416d2affcb5f025515749501fb2fd1c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-03-16 05:11:10 +0100

    Introduccion a Dart - @override

--- 01_dart_intro/07_clases.dart
+++ 01_dart_intro/07_clases.dart
@@ -1,11 +1,19 @@
 void main() {
+  // Llamadas correctas para los constructores 2 y 3
   // La palabra reservada new para crear una clase es innecesaria.
   // final wolverine = new Hero('Logan', 'Regeneración');
-  final Hero wolverine = Hero('Logan', 'Regeneración');
+  //
+  // final Hero wolverine = Hero('Logan', 'Regeneración');
+  //
+  // Llamadas correctas para el constructor 4
+  final Hero wolverine = Hero(name: 'Logan', power: 'Regeneración');
 
   print(wolverine);
   print(wolverine.name);
   print(wolverine.power);
+
+  final Hero pepe = Hero(name: 'Pepe');
+  print(pepe);
 }
 
 // Clase con dos propiedades.
@@ -17,7 +25,7 @@ class Hero {
 
   // Constructor
 
-  // Forma clásica Y ERRONEA de constructor
+  // 1. Forma clásica Y ERRONEA de constructor
   // Vemos que ocurre un error porque la inicialización se hace DESPUES de la construcción del objeto
   // por lo que ES NULO!!!
   //
@@ -26,14 +34,27 @@ class Hero {
   //   power = pPower;
   // }
 
-  // Forma típica en Dart de crear el constructor e inicialización correcta.
+  // 2. Forma típica en Dart de crear el constructor e inicialización correcta.
   //
   // Hero(String pName, String pPower)
   //     : name = pName,
   //       power = pPower;
 
-  // La otra forma CORRECTA de crear un constructor.
+  // 3. Otra forma CORRECTA de crear un constructor.
   // Se inicializan las propiedades automáticamente.
   // Muy usada.
-  Hero(this.name, this.power);
+  // Hero(this.name, this.power);
+
+  // 4. Otra forma CORRECTA de crear un constructor y la más usada.
+  // Los parámetros del constructor son por nombre.
+  // Indicamos cuales son los requeridos.
+  // NOTA: La llamada a este constructor es diferente. Ver arriba.
+  Hero({required this.name, this.power = 'Sin poder'});
+
+  //? Sobreescribiendo un método nativo usando la anotación @override
+  // No es obligatorio, pero si es buena práctica usarlo.
+  @override
+  String toString() {
+    return '$name - $power';
+  }
 }
