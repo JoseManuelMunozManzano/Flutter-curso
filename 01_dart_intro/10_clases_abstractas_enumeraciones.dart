9c5ffb3ec3b603b054f1094dcadc322b33be5816
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-03-16 06:41:30 +0100

    Introduccion a Dart - implements

--- 01_dart_intro/10_clases_abstractas_enumeraciones.dart
+++ 01_dart_intro/10_clases_abstractas_enumeraciones.dart
@@ -1,14 +1,17 @@
 void main() {
   final windPlant = WindPlant(initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);
 
   print('wind: ${chargePhone(windPlant)}');
+  print('nuclear: ${chargePhone(nuclearPlant)}');
 }
 
 enum PlantType { nuclear, wind, water }
 
 abstract class EnergyPlant {
   double energyLeft;
-  PlantType type;
+  final PlantType
+      type; // Una vez definida el tipo de planta jamás va a cambiar.
 
   EnergyPlant({required this.energyLeft, required this.type});
 
@@ -42,7 +45,7 @@ class WindPlant extends EnergyPlant {
 // ¿Por qué crear una clase abstracta si podría haber creado directamente la clase WindPlant?
 // Se hace porque podemos crear métodos, funciones o cualquier otro tipo de estructura en la que no
 // esperamos una WindPlant, sino cualquier EnergyPlant que exista.
-// 
+//
 // La idea es aplicar el principio de inversión de dependencia.
 // Si tenemos nuevas plantas de energía no tenemos que tocar este método, se puede usar tal cual por
 // esa nueva planta de energía, ya que extiende o implementa EnergyPlant.
@@ -53,3 +56,26 @@ double chargePhone(EnergyPlant plant) {
 
   return plant.energyLeft - 10;
 }
+
+// extends o implements
+// - Con implements tenemos que definir de nuevo las propiedades y métodos de la base abstracta.
+//   Hemos indicado la anotación @override en todo (menos el constructor), para dejar claro que
+//   lo estamos sobreescribiendo.
+class NuclearPlant implements EnergyPlant {
+  @override
+  double energyLeft;
+
+  @override
+  final PlantType type = PlantType.nuclear;
+
+  NuclearPlant({required this.energyLeft});
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= (amount * 0.5);
+  }
+}
+
+// Tanto extends como implements sirven para la herencia, pero podemos querer que una clase solo implemente
+// (implements) un método de una clase abstracta en vez de extender (extends) todos los métodos que la clase padre tenga.
+// Podemos decir que extends es más general e implements más específico.
