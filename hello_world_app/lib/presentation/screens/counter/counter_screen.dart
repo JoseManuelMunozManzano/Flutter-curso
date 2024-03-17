d573ae8019660a991c3f517c58c089cbd0107eeb
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-03-17 12:30:50 +0100

    Primeros pasos - Operador ternario

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -55,9 +55,10 @@ class _CounterScreenState extends State<CounterScreen> {
               style:
                   const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            const Text(
-              'Clicks',
-              style: TextStyle(fontSize: 25),
+            Text(
+              // clickCounter == 1 ? 'Click' : 'Clicks',
+              'Click${clickCounter == 1 ? '' : 's'}',
+              style: const TextStyle(fontSize: 25),
             )
           ],
         ),
