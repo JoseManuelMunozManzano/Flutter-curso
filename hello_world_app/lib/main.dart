704fba9490bdcf526e669dd32429128870a0df7f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-03-17 07:07:18 +0100

    Primeros pasos - Scaffold, eliminar banner DEBUG y buenas prácticas

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -13,13 +13,27 @@ void main() {
 // Dejar el cursor sobre MyApp.
 // Pulsar Cmd + .
 // Seleccionar Create 1 missing override
+//
+// El BuildContext, entre otras cosas, crea el árbol de Widgets.
+// El key permite nombrar un Widget dentro del arbol de Widgets.
+//    Para crear rápidamente el key, nos posicionamos en MyApp, pulsamos Cmd + . y seleccionamos Add 'key' to constructors
 class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Este MaterialApp es nuestra aplicación.
-    return MaterialApp(
-      // El Center Widget se encarga de centrar su hijo en las dimensiones que tenga disponibles el padre.
-      home: Center(child: Text('Hola Mundo')),
+    // Indicamos const porque este código NUNCA va a cambiar.
+    return const MaterialApp(
+      // Esto es para que no aparezca en el emulador el banner indicando que estamos en DEBUG
+      debugShowCheckedModeBanner: false,
+
+      // Un Scaffold da la bases de un diseño de material
+      home: Scaffold(
+        // El Center Widget se encarga de centrar su hijo en las dimensiones que tenga disponibles el padre.
+        body: Center(child: Text('Hola Mundo')),
+      )
     );
   }
 }
