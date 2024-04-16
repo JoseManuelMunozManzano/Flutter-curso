f5a5ab1ac53be54872f8df3a9e168bacaf3ccdf6
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-04-16 19:18:35 +0200

    cinemapedia - Realizar el mapeo de la respuesta de MovieDB

--- cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 // Este datasource está especializado en interaccionar con la API de TMDB.
@@ -21,7 +23,15 @@ class MoviedbDatasource extends MoviesDatasource {
     // Cuando mandemos esta solicitud, tenemos que transformar la respuesta a mi entidad.
     // Creamos un modelo para leer la respuesta que viene de TMDB y un mapper para tomar esa data y crear nuesta entidad.
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+    // Las películas sin poster se filtran
+    .where((moviedb) => moviedb.posterPath != '')
+    // El objetivo de este map es crearse la instancia de un Movie.
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+    ).toList();
 
     return movies;
   }
--- cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
+++ cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -17,7 +17,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
         ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-        : 'https://ih1.redbubble.net/image.3224006290.0121/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+        : 'no-poster',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
