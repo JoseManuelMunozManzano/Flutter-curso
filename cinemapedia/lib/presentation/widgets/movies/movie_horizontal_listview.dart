85e09d863df37d57ebbbac72942e56fb685e1e9f
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2024-04-29 07:45:51 +0200

    cinemapedia search - Mostrar calificación de la película

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -150,7 +150,7 @@ class _Slide extends StatelessWidget {
               children: [
                 Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
                 const SizedBox(width: 3,),
-                Text(HumanFormats.number(movie.voteAverage), style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800),),
+                Text(HumanFormats.number(movie.voteAverage, 1), style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800),),
                 // const SizedBox(width: 10,),
                 const Spacer(),
                 Text(HumanFormats.number(movie.popularity), style: textStyles.bodySmall)
